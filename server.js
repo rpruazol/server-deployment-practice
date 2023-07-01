3c53083c04e94ab6bcd1e44b1134840aa756b5fa
rpruazol <rpruazol@gmail.com>
2023-07-01 18:23:02 -0400

    proof of life

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+// server.js will have the routes, but the handler functions will live elsewhere
+
+
+const express = require('express');
+
+const app = express();
+
+module.exports = app;
